c7e6e008fa191ffd485633e09db3734c8f47fd22
JohnnyUtah666 <porksloppp@gmail.com>
2022-10-04 20:16:46 -0700

    write html skeleton, write js logic to create grid, apply minor css styles

---
+++ myscript.js
@@ -0,0 +1,32 @@
+function genDivs(num) {
+    document.querySelector("#container");
+    let e = document.container;
+    for (let i = 0; i < num; i++) {
+        let row = document.createElement("div");
+        row.className = "row";
+        row.style.display = "flex";
+        row.style.flexDirection = "row";
+        row.style.flexWrap = "wrap";
+        row.style.height = "100%";
+        row.style.width = "100%";
+        
+        for (let j = 1; j <= num; j++) {
+            let column = document.createElement("div");
+            column.className = "column";
+            column.style.flexBasis = "100%";
+            column.style.borderStyle = "solid";
+            column.style.borderWidth = "1px";
+            column.style.borderColor = "black";
+            
+            
+            row.appendChild(column);
+        }
+        container.appendChild(row);      
+    }
+}
+
+
+
+genDivs(30);
+
+
